b99304554ba751b100a56cccc3a5d86b7e1994d3
AlexanderChum <Senneiko@yandex.ru>
2025-03-02 11:41:29 +0300

    Review commentaries edits

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -17,11 +16,9 @@ public class Film {
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
-    @NotBlank(message = "Описание не может быть пустым")
     @Size(max = 200, message = "Слишком длинное описание")
     private String description;
 
-    @NotNull(message = "Дата выпуска фильма не может быть пустой")
     @ReleaseDate
     private LocalDate releaseDate;
 

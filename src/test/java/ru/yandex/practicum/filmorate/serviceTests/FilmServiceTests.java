0b10b91b2f94dacd7b966829c6c85c2b5692095e
AlexanderChum <Senneiko@yandex.ru>
2025-03-30 14:31:25 +0300

    Why are we still here? Just to suffer and fix checkstyle?

--- src/test/java/ru/yandex/practicum/filmorate/serviceTests/FilmServiceTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/serviceTests/FilmServiceTests.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.serviceTests;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import lombok.RequiredArgsConstructor;
